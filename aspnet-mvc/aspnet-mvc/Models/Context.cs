38fc2af42e17cc483e595459a924b134f24c62a2
Anderson Simao <simao.anderson4@gmail.com>
2022-05-26 20:19:58 -0300

    aspnetmvc da Dio

---
+++ aspnet-mvc/aspnet-mvc/Models/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace aspnet_mvc.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optBuilder)
+        {
+            optBuilder.UseSqlServer(@"Server=DESKTOP-P0CRGLM;Database=Testemvc;Integrated Security=True");
+        }
+    }
+}
