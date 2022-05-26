65962bb0fdca8fe1d193b1ab7d44ac070cf11683
Anderson Simao <simao.anderson4@gmail.com>
2022-05-26 20:19:58 -0300

    aspnetmvc da Dio

---
+++ aspnet-mvc/aspnet-mvc/Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace aspnet_mvc.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+
+        public List<Produto> Produtos { get; set; }
+    }
+}
---
+++ aspnet-mvc/aspnet-mvc/Models/Produto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace aspnet_mvc.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public int Quantidade { get; set; }
+        public int CategoriaId { get; set; }
+        public Categoria? Categoria { get; set; }
+    }
+}
