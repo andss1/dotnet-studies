6aa3dbe44e6164c19c5e7ad458cc5de63d076bcb
Anderson Simao <simao.anderson4@gmail.com>
2022-05-27 11:26:38 -0300

    Swagger com DI e Rep.Pat.

---
+++ aspnet-di/aspnet-di/Controllers/PrescriptionController.cs
@@ -0,0 +1,28 @@
+﻿using aspnet_di.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_di.Controllers
+{
+    [Route("api/[controller]")]
+    public class PrescriptionController : Controller
+    {
+        private readonly IPrescription _prescription;
+
+        public PrescriptionController(IPrescription prescription)
+        {
+            _prescription = prescription;
+
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var prescriptions = _prescription.GetAll();
+            if( prescriptions == null )
+            {
+                return NotFound();
+            }
+            return Ok(prescriptions);
+        }
+    }
+}
